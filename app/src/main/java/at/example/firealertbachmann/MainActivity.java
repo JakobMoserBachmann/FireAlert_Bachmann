b3510a5a71ce55754f89c424415f827137c6524a
PabloMoser <104075627+PabloMoser@users.noreply.github.com>
2022-12-22 10:48:59 +0100

    NFC Reader implementation & Deleted files

--- app/src/main/java/at/example/firealertbachmann/MainActivity.java
+++ app/src/main/java/at/example/firealertbachmann/MainActivity.java
@@ -1,11 +1,21 @@
 package at.example.firealertbachmann;
 
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.NfcAdapter;
+import android.nfc.tech.MifareClassic;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.widget.ImageView;
 import at.example.firealertbachmann.databinding.ActivityMainBinding;
+import at.example.firealertbachmann.ui.NfcScan.NfcScanFragment;
 
 import com.google.android.material.navigation.NavigationView;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -17,6 +27,11 @@ public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
+    Fragment fragment;
+    NfcAdapter nfcAdapter;
+    PendingIntent pendingIntent;
+    IntentFilter[] intentFiltersArray;
+    String[][] techListsArray;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +52,22 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+
+        if (nfcAdapter != null) {
+
+            pendingIntent = PendingIntent.getActivity(
+                    this, 0, new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP),
+                    PendingIntent.FLAG_MUTABLE);
+
+            IntentFilter filter = new IntentFilter(NfcAdapter.ACTION_TECH_DISCOVERED);
+            techListsArray = new String[][]{new String[]{MifareClassic.class.getName()}};
+            intentFiltersArray = new IntentFilter[]{filter};
+        } else {
+            // Handy unterstützt kein NFC
+        }
     }
 
     @Override
@@ -44,6 +75,55 @@ public class MainActivity extends AppCompatActivity {
 
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+
+        super.onNewIntent(intent);
+        FragmentManager manager = this.getSupportFragmentManager();
+        manager.executePendingTransactions();
+        fragment = manager.findFragmentById(R.id.nav_host_fragment_content_main).getChildFragmentManager().getFragments().get(0);
+
+        // Check if the fragment is an instance of the right fragment
+        if (fragment instanceof NfcScanFragment) {
+            Log.v("HELLO", "HALLLLLLLOOOOOOOO");
+
+            NfcScanFragment my = (NfcScanFragment) fragment;
+            // Pass intent or its data to the fragment's method
+            my.processNFC(intent);
+        }
+        else
+        {
+
+            Log.v("HELLO",  "##########");
+
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        stopNFC();
+    }
+
+    public void stopNFC() {
+        if (nfcAdapter != null) {
+            nfcAdapter.disableForegroundDispatch(this);
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        startNFC();
+    }
+
+    public void startNFC() {
+        if (nfcAdapter != null) {
+            nfcAdapter.enableForegroundDispatch(this, pendingIntent, intentFiltersArray, techListsArray);
+        }
     }
 
     @Override
--- app/src/main/java/at/example/firealertbachmann/ui/NfcScan/NfcScanFragment.java
+++ app/src/main/java/at/example/firealertbachmann/ui/NfcScan/NfcScanFragment.java
@@ -1,18 +1,25 @@
 package at.example.firealertbachmann.ui.NfcScan;
 
+import android.content.Intent;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.MifareClassic;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ProgressBar;
-
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
-import androidx.navigation.ui.AppBarConfiguration;
-
 import com.google.android.material.snackbar.Snackbar;
+import java.io.IOException;
+import at.example.firealertbachmann.MainActivity;
 import at.example.firealertbachmann.R;
 import at.example.firealertbachmann.databinding.FragmentNfcscanBinding;
 import cdflynn.android.library.checkview.CheckView;
@@ -20,7 +27,6 @@ import cdflynn.android.library.checkview.CheckView;
 public class NfcScanFragment extends Fragment {
 
     private FragmentNfcscanBinding binding;
-    private AppBarConfiguration mAppBarConfiguration;
     CheckView check;
     Button button;
     ProgressBar progressbar;
@@ -31,18 +37,16 @@ public class NfcScanFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container,Bundle savedInstanceState) {
 
-
-
         binding = FragmentNfcscanBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
         progressbar = root.findViewById(R.id.progressBar);
+
         button = root.findViewById(R.id.nfc_scan_button);
         check = root.findViewById(R.id.check);
 
         progressbar.setVisibility(View. INVISIBLE);
 
-        button.setText("Start NFC Scan");
 
         button.setOnClickListener(view ->
         {
@@ -51,39 +55,88 @@ public class NfcScanFragment extends Fragment {
 
             if (btnCount%2 == 1)
             {
-                check.setVisibility(View.INVISIBLE);
-                progressbar. setVisibility(View. VISIBLE);
-                check.setVisibility(View.INVISIBLE);
+                button.setText("Stop NFC Scan");
+                ((MainActivity)getActivity()).startNFC();
+                setProgressGIF();
+
+                //Snackbar erstellen
                 Snackbar snackbar = Snackbar.make(view, "NFC Scan ist gestartet", Snackbar.LENGTH_LONG);
                 snackbar.show();
-
-                button.setText("Stop NFC Scan");
             }
             else if(btnCount%2 == 0)
             {
-                progressbar.setVisibility(View. INVISIBLE);
                 button.setText("Start NFC Scan");
-
-                check.setVisibility(View.VISIBLE);
-                check.check();
-
-                Snackbar snackbar = Snackbar.make(view, "Info", Snackbar.LENGTH_LONG);
-                snackbar.setAction(R.string.do_string, new View.OnClickListener()
-                {
-                    @Override
-                    public void onClick(View view)
-                    {
-                        NavHostFragment.findNavController(NfcScanFragment.this)
-                                .navigate(R.id.nav_People);
-                    }
-                });
-                snackbar.show();
+                ((MainActivity)getActivity()).stopNFC();
+                setCheckGIF();
+                CreateSnackBarButton(view);
             }
         });
         return root;
     }
 
 
+    public void processNFC(Intent intent) {
+        Log.v("HALLO", "''#############");
+        Tag tagFromIntent = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+
+        MifareClassic mifareClassic = MifareClassic.get(tagFromIntent);
+        if (mifareClassic != null) {
+            try {
+                mifareClassic.connect();
+                mifareClassic.authenticateSectorWithKeyA(4, new byte[]{0x6d, 0x4b, 0x00, 0x00, 0x01, 0x43});
+                byte[] bytes = mifareClassic.readBlock(16);
+
+                String keyNumber = new String(bytes); // Das hier ist die gesuchte Schlüsselnummer
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            setCheckGIF();
+
+            //GIF Timer
+            Handler handler = new Handler(Looper.getMainLooper()) {
+            @Override
+            public void handleMessage(Message msg) {
+                if (msg.what == 1) {
+                    setProgressGIF();
+                }
+            }
+        };
+        handler.sendEmptyMessageDelayed(1, 1000);
+
+
+    } else  {
+            // kein MIFARE Classic NFC Tag
+        }
+    }
+
+    private void setProgressGIF()
+    {
+        check.setVisibility(View.INVISIBLE);
+        progressbar.setVisibility(View.VISIBLE);
+    }
+
+    private void setCheckGIF()
+    {
+        progressbar.setVisibility(View.INVISIBLE);
+        check.setVisibility(View.VISIBLE);
+        check.check();
+    }
+
+    private void CreateSnackBarButton(View view)
+    {
+        Snackbar snackbar = Snackbar.make(view, "Info", Snackbar.LENGTH_LONG);
+        snackbar.setAction(R.string.do_string, new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                NavHostFragment.findNavController(NfcScanFragment.this)
+                        .navigate(R.id.nav_People);
+            }
+        });
+        snackbar.show();
+    }
 
     @Override
     public void onDestroyView() {
