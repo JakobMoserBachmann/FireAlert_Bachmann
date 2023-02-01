d445dd60902dc0dddaf1ad411c573cef34d80766
Moneyboy <96113821+Marc-559@users.noreply.github.com>
2023-02-01 10:29:14 +0100

    did nothing

--- app/src/main/java/at/example/firealertbachmann/ui/Tutorial/TutorialFragmentPage3.java
+++ app/src/main/java/at/example/firealertbachmann/ui/Tutorial/TutorialFragmentPage3.java
@@ -11,36 +11,35 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
 import at.example.firealertbachmann.R;
-import at.example.firealertbachmann.databinding.FragmentTutorialPage4Binding;
+import at.example.firealertbachmann.databinding.FragmentTutorialPage3Binding;
 
-public class TutorialFragmentPage4 extends Fragment
+public class TutorialFragmentPage3 extends Fragment
 {
-    private FragmentTutorialPage4Binding binding;
+    private FragmentTutorialPage3Binding binding;
     TextView textviewNextStep;
     TextView textViewPreviousStep;
-
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState)
     {
-        binding = FragmentTutorialPage4Binding.inflate(inflater, container, false);
+        binding = FragmentTutorialPage3Binding.inflate(inflater, container, false);
 
         View root = binding.getRoot();
-        textviewNextStep = root.findViewById(R.id.tutorialPag4NextStep);
-        textViewPreviousStep = root.findViewById(R.id.tutorialPage4PreviousStep);
+        textviewNextStep = root.findViewById(R.id.tutorialPage3NextStep);
+        textViewPreviousStep = root.findViewById(R.id.tutorialPage3PreviousStep);
 
         textviewNextStep.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(TutorialFragmentPage4.this)
-                        .navigate(R.id.action_nav_TutorialPage4_to_nav_NfcScanFragment);
+                NavHostFragment.findNavController(TutorialFragmentPage3.this)
+                        .navigate(R.id.action_nav_TutorialPage3_to_nav_TutorialPage4);
             }
         });
 
         textViewPreviousStep.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(TutorialFragmentPage4.this)
-                        .navigate(R.id.action_nav_TutorialPage4_to_nav_TutorialPage3);
+                NavHostFragment.findNavController(TutorialFragmentPage3.this)
+                        .navigate(R.id.action_nav_TutorialPage3_to_nav_TutorialPage2);
             }
         });
         return root;
--- app/src/main/java/at/example/firealertbachmann/ui/Tutorial/TutorialFrgamentPage3.java
+++
@@ -1,6 +0,0 @@
-package at.example.firealertbachmann.ui.Tutorial;
-
-import androidx.fragment.app.Fragment;
-
-public class TutorialFrgamentPage3 extends Fragment {
-}
